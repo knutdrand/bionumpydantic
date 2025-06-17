e16ff9877136acbfa6f12ab9cb8aefed244f00d0
Knut Rand <knutdrand@gmail.com>
2025-06-17 11:14:00 +0200

    test: update conversion test

--- tests/test_bionumpydantic.py
+++ tests/test_bionumpydantic.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 """Tests for `bionumpydantic` package."""
+from typing import List
+
 import numpy as np
 import pytest
 from bionumpy import EncodedRaggedArray
 from bionumpy.util.testing import assert_bnpdataclass_equal, assert_encoded_array_equal
+from npstructures import RaggedArray
 
 from bionumpydantic.bionumpydantic import BNPModel
 
@@ -12,13 +15,17 @@ from bionumpydantic.bionumpydantic import BNPModel
 class Example(BNPModel):
     name: str
     age: int
+    scores: List[float]
+    years: List[int]
+    friends: List[str]
 
-
-@pytest.mark.skip(reason="Not yet implemented")
+@pytest.mark.xfail(reason="Not yet implemented")
 def test_convert_annotations():
     new_annotations = Example.convert_annotations()
     assert new_annotations['name'] == EncodedRaggedArray
     assert new_annotations['age'] == np.ndarray
+    assert new_annotations['scores'] == RaggedArray
+
 
 
 
@@ -30,7 +37,8 @@ def test_pydantic_to_bnpdataclass():
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
     bnp_class = Example.to_bnpdataclass()
-    data = bnp_class(name=['test1', 'test2', 'test13'], age=[25, 30, 35])
+    data = bnp_class(name=['test1', 'test2', 'test13'], age=[25, 30, 35], scores=[[1.0], [3.0, 4.0], [5.0, 6.0]], years=[[2020], [2021, 2022], [2023]],
+                    friends=['Alice', 'Bob', 'Charlie'])
     assert_encoded_array_equal(data.name, ['test1', 'test2', 'test13'])
     #assert_bnpdataclass_equal(data, bnp_class(name=['test1', 'test2', 'test13']))
 
