9dfa5c69d64174206f1a09ddd13bda1aca4f4240
Knut Rand <knutdrand@gmail.com>
2025-06-17 09:56:57 +0200

    test: add test for annotation conversion

--- bionumpydantic/bionumpydantic.py
+++ bionumpydantic/bionumpydantic.py
@@ -1,1 +1,38 @@
 """Main module."""
+from pydantic import BaseModel
+from dataclasses import dataclass, field, make_dataclass
+from bionumpy import bnpdataclass
+
+
+class BNPModel(BaseModel):
+
+    @classmethod
+    def convert_annotations(self) -> dict[str, type]:
+        """
+        Converts the annotations of the Pydantic model to a dictionary.
+        """
+        annotations = self.__annotations__
+        fields = {name: field.annotation for name, field in self.__fields__.items()}
+        return {**annotations, **fields}
+
+    @classmethod
+    def to_dataclass(cls):
+        annotations = cls.__annotations__
+        fields = [(name, f.annotation) for name, f in cls.__fields__.items()]
+        # fields = [(name, field.default if name in cls.__fields__ else field.default_factory) for name in
+        #          annotations]
+
+        return make_dataclass(
+            __class__.__name__,
+            fields,
+            bases=(object,),
+            namespace={'__annotations__': annotations}
+        )
+
+    @classmethod
+    def to_bnpdataclass(cls):
+        """
+        Converts the Pydantic model to a bnpdataclass.
+        """
+        from bionumpy.bnpdataclass import bnpdataclass
+        return bnpdataclass(cls.to_dataclass())
--- tests/test_bionumpydantic.py
+++ tests/test_bionumpydantic.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 
 """Tests for `bionumpydantic` package."""
-
+import numpy as np
 import pytest
+from bionumpy import EncodedRaggedArray
+from bionumpy.util.testing import assert_bnpdataclass_equal, assert_encoded_array_equal
+
+from bionumpydantic.bionumpydantic import BNPModel
 
 
-@pytest.fixture
-def response():
+class Example(BNPModel):
+    name: str
+    age: int
+
+
+@pytest.mark.skip(reason="Not yet implemented")
+def test_convert_annotations():
+    new_annotations = Example.convert_annotations()
+    assert new_annotations['name'] == EncodedRaggedArray
+    assert new_annotations['age'] == np.ndarray
+
+
+
+
+
+def test_pydantic_to_bnpdataclass():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
+    bnp_class = Example.to_bnpdataclass()
+    data = bnp_class(name=['test1', 'test2', 'test13'], age=[25, 30, 35])
+    assert_encoded_array_equal(data.name, ['test1', 'test2', 'test13'])
+    #assert_bnpdataclass_equal(data, bnp_class(name=['test1', 'test2', 'test13']))
+
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
